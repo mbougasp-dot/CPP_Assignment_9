e6b48e8729d1b52b960fa0de6e1aae402f8c8a42
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-12 00:49:43 -0500

    N5

--- parse_input().cpp
+++ parse_input().cpp
@@ -1,6 +1,8 @@
-#include "functions.h"
+#include <string>
 
-std::string parse_input(const std::string& input) {
+using namespace std;
+""
+string parse_input(const string& input) {
     if (input.rfind("0x", 0) == 0 || input.rfind("0X", 0) == 0) {
         return "hex";
     }
