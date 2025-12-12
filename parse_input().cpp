73e5285411b64454148eccc29c180a135b62ccc0
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-12 00:50:52 -0500

    Update parse_input().cpp

--- parse_input().cpp
+++ parse_input().cpp
@@ -1,7 +1,9 @@
 #include <string>
 
 using namespace std;
-""
+
+/// Function to determine if the input is hexadecimal or integer.
+
 string parse_input(const string& input) {
     if (input.rfind("0x", 0) == 0 || input.rfind("0X", 0) == 0) {
         return "hex";
