6665edf5c97447c5c1349ee8d07b03f3b7f83456
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-11 23:52:05 -0500

    Update convert_to_binary.cpp

--- convert_to_binary.cpp
+++ convert_to_binary.cpp
@@ -3,3 +3,21 @@
 #include <bitset>
 #include <sstream>
 
+std::string convert_to_binary(const std::string& type, const std::string& input) {
+    unsigned int number;
+
+    if (type == "hex") {
+        std::stringstream ss;
+        ss << std::hex << input;
+        ss >> number;
+    } else {
+        number = std::stoi(input);
+    }
+
+    std::bitset<32> binary(number);
+
+    std::string binaryStr = binary.to_string();
+    binaryStr.erase(0, binaryStr.find_first_not_of('0'));
+
+    return binaryStr;
+}
